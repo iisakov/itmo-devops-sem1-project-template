fa7d91ac639e0d8d8b38c2fa17cc918941caeb3c
iisakov <isakov.arh@yandex.ru>
2025-01-15 04:10:53 +0300

    [DevOps] - выкмнул всё про docker

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,5 @@
 # Для прерывания скрипта в случае возникновения ошибок
 set -e
 
-docker compose down
-docker image rm itmo-devops-sem1-project-template-backend -f
-docker compose up -d
+./bin/app
+
